61502e1e1429d172de3114465d02a90941193941
Leonardo Souza <llsouza.br@gmail.com>
2020-10-13 18:11:53 -0300

    Criada página de mapas

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {FiPlus} from 'react-icons/fi'
+
+import mapMarkerImg from '../images/map-marker.svg';
 
 function OrphanagesMap(){
     return (
-        <h1>Hello OrphanagesMap</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Logo"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+                <footer>
+                    <strong>São Paulo</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to ="" className='create-orphanage'>
+                <FiPlus size={32} color="FFF" />
+            </Link>
+
+        </div>
     );
 }
 
